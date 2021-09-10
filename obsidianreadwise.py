836e663d4229b4f0be74b0bfb13b51d0e9acb232
Andy <duckworth.andy@gmail.com>
2021-09-10 10:10:38 -0700

    Create function for markdown files list.

--- obsidianreadwise.py
+++ obsidianreadwise.py
@@ -1,31 +1,33 @@
 import os
 import sys
 
-# Identify operating system
-opSys = sys.platform
-# print(opSys)
-
-# Change directory to Obsidian notes based on operating system
-if opSys == 'win32':
-    os.chdir('C:/Users/aduckworth/iCloudDrive/iCloud~md~obsidian/Daily Notes')
-else:
-    pass
-
-# print(os.getcwd())
-
-# Create a list of files in the current directory
-fileList = os.listdir(os.getcwd())
-
-# print(fileList)
-
 # Create an empty list to append markdown files
 mdFiles = []
 
-# Loop through file list and append markdown files to mdFiles list
-for i in fileList:
-    if i.endswith('.md'):
-        mdFiles.append(i)
+def identMDFiles():
+    # Identify operating system
+    opSys = sys.platform
+    # print(opSys)
+
+    # Change directory to Obsidian notes based on operating system
+    if opSys == 'win32':
+        os.chdir('C:/Users/aduckworth/iCloudDrive/iCloud~md~obsidian/Daily Notes')
     else:
         pass
 
-print(mdFiles)
+    # print(os.getcwd())
+
+    # Create a list of files in the current directory
+    fileList = os.listdir(os.getcwd())
+    # print(fileList)
+
+    # Loop through file list and append markdown files to mdFiles list
+    for i in fileList:
+        if i.endswith('.md'):
+            mdFiles.append(i)
+        else:
+            pass
+
+    print(mdFiles)
+
+identMDFiles()
